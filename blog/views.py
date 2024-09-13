cd9efc2a5dc2aaaf4fece62d96370a20e118329b
Sergey <main0serg@gmail.com>
2024-09-13 13:38:10 +0600

    added tags to posts

--- blog/views.py
+++ blog/views.py
@@ -3,18 +3,23 @@ from django.core.mail import send_mail
 from django.core.paginator import Paginator
 from django.shortcuts import get_object_or_404, render
 from django.views.decorators.http import require_POST
+from taggit.models import Tag
 
 from .forms import CommentForm, EmailPostForm
 from .models import Post
 
 
-def post_list(request):
+def post_list(request, tag_slug=None):
     post_list = Post.published.all()
+    tag = None
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        post_list = post_list.filter(tags__in=[tag])
     # Постраничная разбивка с 3 постами на страницу
     paginator = Paginator(post_list, 3)
     page_number = request.GET.get("page", 1)
     posts = paginator.get_page(page_number)
-    return render(request, "blog/post/list.html", {"posts": posts})
+    return render(request, "blog/post/list.html", {"posts": posts, "tag": tag})
 
 
 def post_detail(request, year, month, day, post):
