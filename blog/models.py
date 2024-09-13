28c2cd2f5b34b3ba54e0ee95836ac51e07ea374b
Sergey <main0serg@gmail.com>
2024-09-13 10:48:44 +0600

    added get_absolute_url

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.urls import reverse
 from django.utils import timezone
 
 
@@ -10,26 +11,36 @@ class PublishedManger(models.Manager):
 
 class Post(models.Model):
     class Status(models.TextChoices):
-        DRAFT = 'DF', 'Draft'
-        PUBLISHED = 'PB', 'Published'
+        DRAFT = "DF", "Draft"
+        PUBLISHED = "PB", "Published"
 
     title = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=250, unique=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='%(app_label)s_posts')
+    slug = models.SlugField(max_length=250, unique_for_date="publish")
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="%(app_label)s_posts"
+    )
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
-    status = models.CharField(max_length=2, choices=Status.choices, default=Status.DRAFT)
+    status = models.CharField(
+        max_length=2, choices=Status.choices, default=Status.DRAFT
+    )
 
     objects = models.Manager()
     published = PublishedManger()
 
     class Meta:
-        ordering = ['-publish']
+        ordering = ["-publish"]
         indexes = [
-            models.Index(fields=['-publish']),
+            models.Index(fields=["-publish"]),
         ]
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse(
+            "blog:post_detail",
+            args=[self.publish.year, self.publish.month, self.publish.day, self.slug],
+        )
